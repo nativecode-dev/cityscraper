2598a43f8495882c9f77d73f84c24305af202971
mikepham <contact@mikepham.com>
2017-10-10 11:20:39 -0400

    initial

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+const gulp = require('gulp')
+const plugins = require('gulp-load-plugins')()
+
+const $ = plugins.configly(__dirname, 'package.json')
+
+gulp.task('build', ['build:js', 'build:json'])
+
+gulp.task('build:js', () => {
+  return gulp.src($.sources.js)
+    .pipe(plugins.debug($.debug.js))
+    .pipe(plugins.babel($.plugins.babel))
+    .pipe(gulp.dest($.destination.lib))
+})
+
+gulp.task('build:json', () => {
+  return gulp.src($.sources.json)
+    .pipe(plugins.debug($.debug.json))
+    .pipe(gulp.dest($.destination.lib))
+})
+
+gulp.task('clean', () => {
+  return gulp.src($.destination.lib)
+    .pipe(plugins.clean())
+})
+
+gulp.task('default', ['build'])
