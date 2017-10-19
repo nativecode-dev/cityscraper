8bbf79a2787f2a65c350e224223c9fc48c611cfe
Mike Pham <mike.pham@proplogix.com>
2017-10-19 16:33:31 -0400

    checkpoint

--- src/start-logging.ts
+++ src/start-logging.ts
@@ -1,4 +1,5 @@
 import * as cluster from 'cluster'
+import * as os from 'os'
 import { Lincoln, Log } from '@nofrills/lincoln'
 import { Connection } from 'amqp-ts'
 import { Rabbit, RabbitQueue } from './queue'
@@ -9,48 +10,43 @@ import * as fs from 'fs'
 import * as path from 'path'
 
 if (cluster.isMaster && process.env.NODE_ENV !== 'debug') {
+
   console.log(`master process ${process.pid}`)
   cluster.fork()
   cluster.on('exit', () => cluster.fork())
+
 } else if (cluster.isWorker || process.env.NODE_ENV === 'debug') {
+
   console.log(`worker process ${process.pid}`)
+  const logpath = cl('cityscraper').app.local()
+  const logfile = process.argv.length === 3
+    ? path.join(logpath, process.argv[2])
+    : path.join(logpath, `cityscraper-${process.pid}.log`)
+
+  const options = { encoding: 'utf8', flags: 'a+' }
+  const stream = fs.createWriteStream(logfile, options)
+  process.on('exit', () => stream.close())
+
   Promise.resolve(true).then(async () => {
-    const logpath = cl('cityscraper').app.local()
-    let logfile = path.join(logpath, `cityscraper-${process.pid}.log`)
-
-    if (process.argv.length === 3) {
-      logfile = path.join(logpath, process.argv[2])
-    }
-
     const log = Logger.extend('logging')
     const queue = await Rabbit.queue<Log>('logging')
-
-    let newfile = true
 
     if (fs.existsSync(logpath) === false) {
       fs.mkdirSync(logpath)
     }
 
     queue.message((body: Log, properties: any): Promise<any> => {
-      console.log('logging', body)
       const json = JSON.stringify(body)
-      const separator = newfile ? '' : ',\n'
+      const buffer = new Buffer(json, 'utf8')
 
       return new Promise<any>((resolve, reject) => {
-        fs.appendFile(logfile, new Buffer(`${separator}${json}`), (error) => {
-          if (error) {
-            reject(error)
-            return
-          }
-
-          DebugLog(body)
-          resolve(body)
-        })
-        newfile = false
+        console.log('logging', json)
+        stream.write(buffer, () => resolve())
       })
     })
   }).catch((error) => {
     console.log(error)
     process.exit(1)
   })
+
 }
--- src/start-scraping.ts
+++ src/start-scraping.ts
@@ -5,10 +5,13 @@ import { Rabbit, RabbitQueue } from './queue'
 import { SiteScraper } from './scraping'
 
 if (cluster.isMaster && process.env.NODE_ENV !== 'debug') {
+
   console.log(`master process ${process.pid}`)
   cluster.fork()
   cluster.on('exit', () => cluster.fork())
+
 } else if (cluster.isWorker || process.env.NODE_ENV === 'debug') {
+
   console.log(`worker process ${process.pid}`)
   Promise.resolve(true).then(async () => {
     const id = `scraper:${process.pid}`
@@ -16,12 +19,14 @@ if (cluster.isMaster && process.env.NODE_ENV !== 'debug') {
     const queue = await Rabbit.inbox<Site>('scraping')
 
     queue.message(async (body: Site, properties: any): Promise<any> => {
-      console.log(`processing message`, body)
+      console.log('message', body)
       const result = await SiteScraper(body)
+      log.debug('processed', body, result)
       return result
     })
   }).catch((error) => {
     console.log(error)
     process.exit(1)
   })
+
 }
